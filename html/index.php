934e99865055d44926a0ba93dd7c90cb4dbb8e62
liamkinne <kinneliam@gmail.com>
2017-12-28 02:39:19 +1100

    added source link to footer

--- html/index.php
+++ html/index.php
@@ -12,6 +12,7 @@
 		<p>Contents</p>
 	</content>
 	<footer>
+		<p><a href="https://github.com/liamkinne/website" target="_blank">Source</a></p>
 		<p>Copyright © 2017 Liam Kinne</p>
 	</footer>
 </body>
