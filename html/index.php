0234fdecb31898d6f7299bedc3442b6252d65dd7
liamkinne <kinneliam@gmail.com>
2018-01-04 07:34:35 +1100

    added title for posts

--- html/index.php
+++ html/index.php
@@ -1,7 +1,34 @@
+<?php
+// Sanitize HTML chars
+$post_name = htmlspecialchars($_GET['p'], ENT_QUOTES, 'UTF-8');
+// Strip "." to further mitigate the risk of bad users looking at restricted files/content
+$post_name = mb_ereg_replace("\.", "", $post_name);
+
+// If post_name is empty, we must be looking for home
+if ($post_name == "")
+	$post_name = "home";
+
+// Create file name from name
+$post_filename = './posts/' . $post_name . '.md';
+
+// die if file doesn't exist
+if (!file_exists($post_filename))
+{
+	http_response_code(404);
+	include('./error.php'); // TODO: make 404.
+	die;
+}
+
+// Get content from post or homepage
+$post_content = file_get_contents($post_filename, true);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
+	<title>liamkinne<?php if($post_name != "home") echo("/" . $post_name); ?></title>
 	<?php include("includes/head.php"); ?>
+
 </head>
 <body>
 	<header>
@@ -9,29 +36,6 @@
 	</header>
 	<content>
 		<?php
-			// Sanitize HTML chars
-			$post_name = htmlspecialchars($_GET['p'], ENT_QUOTES, 'UTF-8');
-			// Strip "." to further mitigate the risk of bad users looking at restricted files/content
-			$post_name = mb_ereg_replace("\.", "", $post_name);
-
-			// If post_name is empty, we must be looking for home
-			if ($post_name == "")
-				$post_name = "home";
-
-			// Create file name from name
-			$post_filename = './posts/' . $post_name . '.md';
-
-			// die if file doesn't exist
-			if (!file_exists($post_filename))
-			{
-				http_response_code(404);
-				include('./error.php'); // TODO: make 404.
-				die;
-			}
-
-			// Get content from post or homepage
-			$post_content = file_get_contents($post_filename, true);
-
 			// Display using parsedown
 			require("parsedown/Parsedown.php");
 			echo Parsedown::instance()->text($post_content);
