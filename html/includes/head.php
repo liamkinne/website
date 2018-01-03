6b698212bce9319e5991d0560d0e0a6d38da033a
liamkinne <kinneliam@gmail.com>
2018-01-04 08:59:28 +1100

    remove google identifier

--- html/includes/head.php
+++ html/includes/head.php
@@ -1,14 +1,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111895228-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-111895228-1');
-</script>
 <!-- Twitter Card -->
 <meta name="twitter:card" content="summary">
 <meta name="twitter:site" content="@liamkinne">
