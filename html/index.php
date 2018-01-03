1fb94ed4303b81f91d071d39a64550da0cef18a4
liamkinne <kinneliam@gmail.com>
2018-01-04 03:45:41 +1100

    Collated post.php functionality to index.php

--- html/index.php
+++ html/index.php
@@ -9,8 +9,36 @@
 	</header>
 	<content>
 		<?php
-		include("parsedown/Parsedown.php");
-		echo Parsedown::instance()->text(file_get_contents("./posts/home.md", true));
+			// Sanitize HTML chars
+			$post_name = htmlspecialchars($_GET['p'], ENT_QUOTES, 'UTF-8');
+			// Strip "." to further mitigate the risk of bad users looking at restricted files/content
+			$post_name = mb_ereg_replace("\.", "", $post_name);
+
+			// If post_name is empty, we must be looking for home
+			if ($post_name == "")
+				$post_name = "home";
+
+			// Create file name from name
+			$post_filename = './posts/' . $post_name . '.md';
+
+			// die if file doesn't exist
+			if (!file_exists($post_filename))
+			{
+				http_response_code(404);
+				include('./error.php'); // TODO: make 404.
+				die;
+			}
+
+			// Get content from post or homepage
+			$post_content = file_get_contents($post_filename, true);
+
+			// Display using parsedown
+			require("parsedown/Parsedown.php");
+			echo Parsedown::instance()->text($post_content);
+
+			// Display file modification time in not on homepage
+			if ($post_name != "home")
+				echo("<p class='file-time'>Last modified: January 1st, 1970 + " . filemtime($post_filename));
 		?>
 	</content>
 	<footer>
