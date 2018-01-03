a54f8caea8c25a79b41acefb6ef3b0f743691043
liamkinne <kinneliam@gmail.com>
2018-01-04 06:55:09 +1100

    Added crypto currency addresses

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -1,2 +1,8 @@
+<pre>
+BTC 156TcXSPYSE5GUnEWPUjENauBZAUPC54bc
+BCH 135bCchzVJLAWV7Q4R3QL3CufWXyqpFVbs
+ETH 0xB44b3411aA9deC6daAa45BE246B952e84E2e65EF
+LTC LYMfxhvn5duhJbfVKxN33yYSeMUMCZXXUD
+</pre>
 <p><a href="https://github.com/liamkinne/website" target="_blank">Source</a></p>
 <p>Copyright © 2017 Liam Kinne</p>
